628aa89a01fa33e5f5753aeb07f88fb32fda365c
Sepang2 <sepang_2@naver.com>
2023-11-30 09:40:51 +0900

    animate css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <img
+        className="w-96 animate__animated animate__tada animate__fast animate__infinite"
+        src="./images/cat.webp"
+        alt="cat"
+      />
+    </div>
+  );
 };
 
 export default App;
